7873f1e2d7533e3797a58bc7f3e6f037aa3c6864
Vitaly Trakhtenberg <webdev@tca.by>
2024-11-02 13:13:28 +0300

    Changed method validateSensorState

--- src/SensorValidator.php
+++ src/SensorValidator.php
@@ -2,13 +2,23 @@
 
 namespace src;
 
+use Psr\Container\ContainerInterface;
+use src\SensorFace;
+
 class SensorValidator
 {
+    private static $logger;
+
+    public static function setContainer(ContainerInterface $container)
+    {
+        self::$logger = $container->get('logger');
+    }
+
     public static function validate(&$sensorDetails): bool
     {
-        global $logger;
-        $logger->debug("SensorValidator::validate()");
-        $logger->info("input", ["sensorDetails" => $sensorDetails]);
+        self::$logger->debug("SensorValidator::validate()");
+        self::$logger->info("input", ["sensorDetails" => $sensorDetails]);
+
         if (!is_array($sensorDetails)) {
             $sensorDetails = [];
         }
@@ -24,7 +34,8 @@ class SensorValidator
         }
 
         $sensorDetails = array_intersect_key($sensorDetails, $requiredKeys);
-        $logger->info("modified", ["sensorDetails" => $sensorDetails]);
+
+        self::$logger->info("modified", ["sensorDetails" => $sensorDetails]);
 
         return self::validateSensorId($sensorDetails['sensorId']) &&
             self::validateSensorFace($sensorDetails['sensorFace']) &&
@@ -33,27 +44,27 @@ class SensorValidator
 
     public static function validateSensorId($sensorId): bool
     {
-        global $logger;
         $res = filter_var($sensorId, FILTER_VALIDATE_INT) !== false &&
             $sensorId >= 10000 && $sensorId <= 99999;
-        $logger->info("validateSensorId:", ["sensorId" => $sensorId, "result" => $res]);
+        self::$logger->info("validateSensorId:", ["sensorId" => $sensorId, "result" => $res]);
+
         return $res;
     }
 
     public static function validateSensorFace($sensorFace): bool
     {
-        global $logger;
         $res=SensorFace::tryFrom($sensorFace) !== null ||
             in_array($sensorFace, array_column(SensorFace::cases(), 'value'), true);
-        $logger->info("validateSensorFace:", ["sensorFace" => $sensorFace, "result" => $res]);
+        self::$logger->info("validateSensorFace:", ["sensorFace" => $sensorFace, "result" => $res]);
+
         return $res;
     }
 
     public static function validateSensorState($sensorState): bool
     {
-        global $logger;
-        $res=filter_var($sensorState, FILTER_VALIDATE_BOOLEAN) !== false;
-        $logger->info("validateSensorState:", ["sensorState" => $sensorState, "result" => $res]);
+        $res=in_array($sensorState, [true, false, 1, 0, "1", "0"], true);
+        self::$logger->info("validateSensorState:", ["sensorState" => $sensorState, "result" => $res]);
+
         return $res;
     }
 }
