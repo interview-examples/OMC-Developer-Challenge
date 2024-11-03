9feb3918022b9e32a94e68ba52ef562cd4925be6
Vitaly Trakhtenberg <webdev@tca.by>
2024-11-03 19:43:03 +0300

    Changed method validateSensorDetails Just 2 fields could be getting from a human/http-request; redundant checked fields removed.

--- src/sensors/SensorValidator.php
+++ src/sensors/SensorValidator.php
@@ -22,7 +22,9 @@ class SensorValidator
         $required_keys = [
             'sensorId' => null,
             'sensorFace' => null,
-            'sensorState' => false,
+            //'sensorState' => false,
+            //'isSensorOutlier' => false,
+            //'sensorLastUpdate' => null,
         ];
         foreach ($required_keys as $key => $default_value) {
             if (!isset($sensor_details[$key])) {
@@ -32,18 +34,17 @@ class SensorValidator
 
         $sensor_details = array_intersect_key($sensor_details, $required_keys);
 
-        self::$logger->info("modified", ["sensorDetails" => $sensor_details]);
+        self::$logger->debug("modified", ["sensorDetails" => $sensor_details]);
 
         return self::validateSensorId($sensor_details['sensorId']) &&
-            self::validateSensorFace($sensor_details['sensorFace']) &&
-            self::validateSensorState($sensor_details['sensorState']);
+            self::validateSensorFace($sensor_details['sensorFace']);
     }
 
     public static function validateSensorId($sensor_id): bool
     {
         $res = filter_var($sensor_id, FILTER_VALIDATE_INT) !== false &&
             $sensor_id >= 10000 && $sensor_id <= 99999;
-        self::$logger->info("validateSensorId:", ["sensorId" => $sensor_id, "result" => $res]);
+        self::$logger->debug("validateSensorId:", ["sensorId" => $sensor_id, "result" => $res]);
 
         return $res;
     }
@@ -56,7 +57,7 @@ class SensorValidator
             $res=SensorFace::tryFrom($sensor_face) !== null ||
                 in_array($sensor_face, array_column(SensorFace::cases(), 'value'), true);
         }
-        self::$logger->info("validateSensorFace:", ["sensorFace" => $sensor_face, "result" => $res]);
+        self::$logger->debug("validateSensorFace:", ["sensorFace" => $sensor_face, "result" => $res]);
 
         return $res;
     }
@@ -64,15 +65,14 @@ class SensorValidator
     public static function validateSensorState($sensor_state): bool
     {
         $res=in_array($sensor_state, [true, false, 1, 0, "1", "0"], true);
-        self::$logger->info("validateSensorState:", ["sensorState" => $sensor_state, "result" => $res]);
+        self::$logger->debug("validateSensorState:", ["sensorState" => $sensor_state, "result" => $res]);
 
         return $res;
     }
 
     public static function validateTemperatureData(array $temperature_data)
     {
-        self::$logger->debug("SensorValidator::validateTemperatureData()");
-        self::$logger->info("input", ["sensorDetails" => $temperature_data]);
+        self::$logger->info("SensorValidator::validateTemperatureData():input", ["sensorDetails" => $temperature_data]);
 
         if (!is_array($temperature_data)) {
             $temperature_data = [];
@@ -90,8 +90,6 @@ class SensorValidator
 
         $temperature_data = array_intersect_key($temperature_data, $required_keys);
 
-        self::$logger->info("modified", ["temperatureData" => $temperature_data]);
-
         return self::validateSensorId($temperature_data['sensorId']) &&
             self::validateTimestamp($temperature_data['timestamp']) &&
             self::validateTemperature($temperature_data['temperature']);
@@ -102,7 +100,7 @@ class SensorValidator
         $res = filter_var($timestamp, FILTER_VALIDATE_INT) !== false
             && ($timestamp <= PHP_INT_MAX)
             && ($timestamp >= ~PHP_INT_MAX);
-        self::$logger->info("validateTimestamp:", ["timestamp" => $timestamp, "result" => $res]);
+        self::$logger->debug("validateTimestamp:", ["timestamp" => $timestamp, "result" => $res]);
 
         return $res;
     }
@@ -110,7 +108,7 @@ class SensorValidator
     private static function validateTemperature($temperature): bool
     {
         $res = filter_var($temperature, FILTER_VALIDATE_FLOAT) !== false;
-        self::$logger->info("validateTemperature:", ["temperature" => $temperature, "result" => $res]);
+        self::$logger->debug("validateTemperature:", ["temperature" => $temperature, "result" => $res]);
 
         return $res;
     }
